f20c71b1b2cbbed922321a657e977457d2810566
Velin Dankov <velin80@web.de>
2025-07-09 16:19:24 +0300

    feat: Update login and register components with proper titles and form handling

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,18 @@
 import { Component } from "@angular/core";
+import { FormsModule, NgForm } from "@angular/forms";
 import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-login",
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    FormsModule, // template driven approach
+  ],
   templateUrl: "./login.component.html",
   styleUrl: "./login.component.css",
 })
-export class LoginComponent {}
+export class LoginComponent {
+  formSumbitHandler(form: NgForm) {
+    console.log(form.value);
+  }
+}
