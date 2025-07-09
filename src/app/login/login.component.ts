36cd5e8ad281bebfefbcc974164890b27a26d70a
Velin Dankov <velin80@web.de>
2025-07-09 17:42:17 +0300

    feat: Refactor login form handling and validation logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { JsonPipe } from "@angular/common";
+import { Component, ViewChild } from "@angular/core";
 import { FormsModule, NgForm } from "@angular/forms";
 import { RouterLink } from "@angular/router";
 
@@ -7,12 +8,25 @@ import { RouterLink } from "@angular/router";
   imports: [
     RouterLink,
     FormsModule, // template driven approach
+    JsonPipe,
   ],
   templateUrl: "./login.component.html",
   styleUrl: "./login.component.css",
 })
 export class LoginComponent {
-  formSumbitHandler(form: NgForm) {
-    console.log(form.value);
+  @ViewChild("loginForm") form: NgForm | undefined;
+  formSumbitHandler() {
+    if (!this.form) {
+      return;
+    }
+
+    const form = this.form;
+    if (form?.invalid) {
+      console.log("ivalid", form.invalid);
+    }
+
+    const { email, password } = form.value;
+    console.log(email);
+    console.log(password);
   }
 }
